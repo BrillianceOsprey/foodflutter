83c17dfa7ae6b8e77c7d6f944f6fc9118cedc7db
Admin <admin@Admins-MacBook-Pro.local>
2023-03-11 19:26:03 +0630

    foof to foodflutter text change

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -21,7 +21,7 @@ class _HomeState extends State<Home> {
           ),
           children: [
             TextSpan(
-              text: "Food",
+              text: "Food flutter",
               style: TextStyle(
                 color: Colors.white
               )
